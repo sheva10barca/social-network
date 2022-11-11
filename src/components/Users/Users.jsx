82bb7bd8d99dd0aea7c5ae5e183770d61ebefb58
sheva <vladislavshevchenko10@gmail.com>
2022-11-11 15:57:30 +0200

    added componentDidMount in Users

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -5,13 +5,10 @@ import styles from "./users.module.css";
 import userPhoto from "../../assets/images/user.png";
 
 class Users extends React.Component {
-   constructor(props) {
-      super(props);
-      if (props.users.length === 0) {
-         axios.get("https://social-network.samuraijs.com/api/1.0/users").then((response) => {
-            props.setUsers(response.data.items);
-         });
-      }
+   componentDidMount() {
+      axios.get("https://social-network.samuraijs.com/api/1.0/users").then((response) => {
+         this.props.setUsers(response.data.items);
+      });
    }
 
    render() {
