79b5960bc035e054864000ffefcff7396fcb0419
sheva <vladislavshevchenko10@gmail.com>
2022-11-14 13:45:01 +0200

    added withRouter

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
          <div className="app-wrapper-content">
             <Routes>
                <Route path="/dialogs/*" element={<DialogsContainer />} />
-               <Route path="/profile/*" element={<ProfileContainer />} />
+               <Route path="/profile/:userId?" element={<ProfileContainer />} />
                <Route path="/users" element={<UsersContainer />} />
             </Routes>
          </div>
