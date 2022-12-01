472176b4704a017ccf5262cd57b1544449f80c32
sheva <vladislavshevchenko10@gmail.com>
2022-12-01 17:01:43 +0200

    paginator update

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,7 +4,7 @@ import User from "./User";
 let Users = ({ currentPage, totalUsersCount, pageSize, onPageChanged, users, ...props }) => {
    return (
       <div>
-         <Paginator currentPage={currentPage} onPageChanged={onPageChanged} totalUsersCount={totalUsersCount} pageSize={pageSize} />
+         <Paginator currentPage={currentPage} onPageChanged={onPageChanged} totalItemsCount={totalUsersCount} pageSize={pageSize} />
          <div>
             {users.map((u) => (
                <User user={u} followingInProgress={props.followingInProgress} key={u.id} unfollow={props.unfollow} follow={props.follow} />
--- src/components/common/Paginator/Paginator.jsx
+++ src/components/common/Paginator/Paginator.jsx
@@ -1,30 +1,62 @@
+import { useState } from "react";
+import cn from "classnames";
+
 import styles from "./Paginator.module.css";
 
-let Paginator = ({ totalUsersCount, pageSize, currentPage, onPageChanged }) => {
-   let pagesCount = Math.ceil(totalUsersCount / pageSize);
+let Paginator = ({ totalItemsCount, pageSize, currentPage, onPageChanged, portionSize = 10 }) => {
+   let pagesCount = Math.ceil(totalItemsCount / pageSize);
 
    let pages = [];
    for (let i = 1; i <= pagesCount; i++) {
-      if (pages.length < 10) {
-         pages.push(i);
-      }
+      pages.push(i);
    }
 
+   let portionCount = Math.ceil(pagesCount / portionSize);
+   let [portionNumber, setPortionNumber] = useState(1);
+   let leftPortionPageNumber = (portionNumber - 1) * portionSize + 1;
+   let rightPortionPageNumber = portionNumber * portionSize;
+
    return (
-      <div>
-         {pages.map((p, id) => {
-            return (
-               <span
-                  className={currentPage === p ? styles.selectedPage : ""}
-                  onClick={(e) => {
-                     onPageChanged(p);
-                  }}
-                  key={id}
-               >
-                  {p}
-               </span>
-            );
-         })}
+      <div className={styles.paginator}>
+         {portionNumber > 1 && (
+            <button
+               onClick={() => {
+                  setPortionNumber(portionNumber - 1);
+               }}
+            >
+               PREV
+            </button>
+         )}
+
+         {pages
+            .filter((p) => p >= leftPortionPageNumber && p <= rightPortionPageNumber)
+            .map((p) => {
+               return (
+                  <span
+                     className={cn(
+                        {
+                           [styles.selectedPage]: currentPage === p,
+                        },
+                        styles.pageNumber,
+                     )}
+                     key={p}
+                     onClick={(e) => {
+                        onPageChanged(p);
+                     }}
+                  >
+                     {p}
+                  </span>
+               );
+            })}
+         {portionCount > portionNumber && (
+            <button
+               onClick={() => {
+                  setPortionNumber(portionNumber + 1);
+               }}
+            >
+               NEXT
+            </button>
+         )}
       </div>
    );
 };
