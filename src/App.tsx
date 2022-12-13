7b5d9dcea1017e782d7427afdfa8d7cfc5f93a66
sheva <vladislavshevchenko10@gmail.com>
2022-12-13 12:29:50 +0200

    added ant design

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,4 @@
 import React, { Suspense } from "react";
-// @ts-ignore
-import HeaderContainer from "./components/Header/HeaderContainer.tsx";
-// @ts-ignore
-import Navbar from "./components/Navbar/Navbar.tsx";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { compose } from "redux";
 import { connect, Provider } from "react-redux";
@@ -16,10 +12,19 @@ import { AppStateType } from "./redux/redux-store";
 import { UsersPage } from "./components/Users/UsersContainer.tsx";
 // @ts-ignore
 import { LoginPage } from "./components/Login/LoginPage.tsx";
+import { Link } from "react-router-dom";
+// @ts-ignore
+import { Header } from "./components/Header/Header.tsx";
 
 import "./App.css";
 // @ts-ignore
 import Preloader from "./components/common/Preloader/Preloader.tsx";
+import { Layout } from "antd";
+import Breadcrumb from "antd/es/breadcrumb";
+import Menu, { MenuProps } from "antd/es/menu";
+import { LaptopOutlined, SettingOutlined, UserOutlined } from "@ant-design/icons";
+
+
 
 // @ts-ignore
 const DialogsContainer = React.lazy(() => import("./components/Dialogs/DialogsContainer.tsx"));
@@ -28,7 +33,34 @@ const ProfileContainer = React.lazy(() => import("./components/Profile/ProfileCo
 
 type MapPropsType = ReturnType<typeof mapStateToProps>;
 type DispatchPropsType = { initializeApp: () => void };
+const { Content, Footer, Sider } = Layout;
+export type MenuItem = Required<MenuProps>["items"][number];
 
+export function getItem(label: React.ReactNode, key?: React.Key | null, icon?: React.ReactNode, children?: MenuItem[]): MenuItem {
+   return {
+      key,
+      icon,
+      children,
+      label,
+   } as MenuItem;
+}
+
+const itemsSideMenu: MenuItem[] = [
+   getItem("My Profile", "MyProfile", <UserOutlined />, [
+      getItem(<Link to="/profile">Profile</Link>, "Profile"),
+      getItem(<Link to="/dialogs">Messages</Link>, "Messages"),
+   ]),
+
+   getItem("Developers", "Developers", <LaptopOutlined />, [
+      getItem(<Link to="/developers">Developers list</Link>, "DevelopersList"),
+      getItem(<Link to="/chat">Developers chat</Link>, "DevelopersChat"),
+   ]),
+
+   getItem("Settings", "Settings", <SettingOutlined />, [
+      getItem(<Link to="/news">News</Link>, "News"),
+      getItem(<Link to="/music">Music</Link>, "Music"),
+   ]),
+];
 class App extends React.Component<MapPropsType & DispatchPropsType> {
    catchAllUnhandledErrors = (e: PromiseRejectionEvent) => {
       alert("Some error occured");
@@ -45,33 +77,67 @@ class App extends React.Component<MapPropsType & DispatchPropsType> {
    }
 
    render() {
-      if (!this.props.initialized) {
-         return <Preloader />;
-      }
       return (
-         <div className="app-wrapper">
-            <HeaderContainer />
-            <Navbar />
-            <div className="app-wrapper-content">
-               <Suspense
-                  fallback={
-                     <div>
-                        <Preloader />
-                     </div>
-                  }
-               >
-                  <Routes>
-                     <Route path="/dialogs/*" element={<DialogsContainer />} />
-                     <Route path="/profile/" element={<ProfileContainer />} />
-                     <Route path="/profile/:userId" element={<ProfileContainer />} />
-                     <Route path="/" element={<Navigate to="/profile" />} />
-                     <Route path="/users/" element={<UsersPage pageTitle={"Самураи"} />} />
-                     <Route path="/login/" element={<LoginPage />} />
-                     <Route path="*" element={<div>404 NOT FOUND</div>} />
-                  </Routes>
-               </Suspense>
-            </div>
-         </div>
+         <Layout >
+            <Header />
+            <Content style={{ padding: "0 50px" }}>
+               <Breadcrumb style={{ margin: "16px 0" }}>
+                  <Breadcrumb.Item>
+                     <Link to="/">Home</Link>
+                  </Breadcrumb.Item>
+                  <Breadcrumb.Item>
+                     <Link to="/developers">List</Link>
+                  </Breadcrumb.Item>
+                  <Breadcrumb.Item>App</Breadcrumb.Item>
+               </Breadcrumb>
+               <Layout className="site-layout-background" style={{ padding: "24px 0" }}>
+                  <Sider className="site-layout-background" width={200}>
+                     <Menu mode="inline" style={{ height: "100%" }} items={itemsSideMenu} />
+                  </Sider>
+                  <Content style={{ padding: "0 24px", minHeight: 280 }}>
+                     <Suspense fallback={<Preloader />}>
+                        <Routes>
+                           <Route path="/dialogs/*" element={<DialogsContainer />} />
+                           <Route path="/profile/" element={<ProfileContainer />} />
+                           <Route path="/profile/:userId" element={<ProfileContainer />} />
+                           <Route path="/" element={<Navigate to="/profile" />} />
+                           <Route path="/developers" element={<UsersPage pageTitle={"Developers"} />} />
+                           <Route path="/login/" element={<LoginPage />} />
+                           <Route path="*" element={<div>404 NOT FOUND</div>} />
+                        </Routes>
+                     </Suspense>
+                  </Content>
+               </Layout>
+            </Content>
+            <Footer style={{ textAlign: "center", marginBottom: "10px" }}>
+               <div className="created-by">
+                  Developers Social Network ©2022 Created by Sheva</div>
+            </Footer>
+         </Layout>
+
+         // <div className="app-wrapper">
+         //    <HeaderContainer />
+         //    <Navbar />
+         //    <div className="app-wrapper-content">
+         //       <Suspense
+         //          fallback={
+         //             <div>
+         //                <Preloader />
+         //             </div>
+         //          }
+         //       >
+         //          <Routes>
+         //             <Route path="/dialogs/*" element={<DialogsContainer />} />
+         //             <Route path="/profile/" element={<ProfileContainer />} />
+         //             <Route path="/profile/:userId" element={<ProfileContainer />} />
+         //             <Route path="/" element={<Navigate to="/profile" />} />
+         //             <Route path="/users/" element={<UsersPage pageTitle={"Самураи"} />} />
+         //             <Route path="/login/" element={<LoginPage />} />
+         //             <Route path="*" element={<div>404 NOT FOUND</div>} />
+         //          </Routes>
+         //       </Suspense>
+         //    </div>
+         // </div>
       );
    }
 }
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,31 +1,70 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
 // @ts-ignore
-import s from "./Header.module.css";
+import { selectCurrentUserLogin, selectIsAuth } from "../../redux/auth-selectors.ts";
+import { Avatar, Button, Col, Layout, Menu, Row } from "antd";
+import { useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+// @ts-ignore
+import { logout } from "../../redux/auth-reducer.ts";
+import { UserOutlined } from '@ant-design/icons'
+// @ts-ignore
+export type MapPropsType = {};
 
-export type MapPropsType = {
-   isAuth: boolean;
-   login: string | null;
-};
-export type DispatchPropsType = {
-   logout: () => void;
-};
+export const Header: React.FC<MapPropsType> = (props) => {
+   const isAuth = useSelector(selectIsAuth);
+   const login = useSelector(selectCurrentUserLogin);
 
-const Header: React.FC<MapPropsType & DispatchPropsType> = (props) => {
+   const dispatch = useDispatch();
+
+   const logoutCallback = () => {
+      dispatch(logout());
+   };
+ 
+   const { Header } = Layout;
    return (
-      <header className={s.header}>
-         <img src="https://upload.wikimedia.org/wikipedia/commons/0/0f/Pepsi_logo_2014.svg" alt="" />
-         <div className={s.loginBlock}>
-            {props.isAuth ? (
-               <div>
-                  {props.login} - <button onClick={props.logout}>Log out</button>{" "}
-               </div>
+      <Header className="header">
+         <Row>
+            <Col span={22}>
+               <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["2"]}>
+                  <Menu.Item key="1">
+                     <Link to="/developers">Developers</Link>
+                  </Menu.Item>
+               </Menu>
+            </Col>
+            {isAuth ? (
+               <>
+                  {" "}
+                  <Col span={1}>
+                     <Avatar alt={login || ''} style={{ backgroundColor: "#87d068" }} icon={<UserOutlined />} />
+                  </Col>
+                  <Col span={1}>
+                     <Button onClick={logoutCallback}>Log out</Button>
+                  </Col>
+               </>
             ) : (
-               <NavLink to={"/login"}>Login</NavLink>
+               <Col span={2}>
+                  <Button>
+                     <Link to={"/login"}>Log in</Link>
+                  </Button>
+               </Col>
             )}
-         </div>
-      </header>
+         </Row>
+      </Header>
+
+      // <header className={s.header}>
+      //    <img src="https://upload.wikimedia.org/wikipedia/commons/0/0f/Pepsi_logo_2014.svg" alt="" />
+      //    <div className={s.loginBlock}>
+      //       {props.isAuth ? (
+      //          <div>
+      //             {props.login} - <button onClick={props.logout}>Log out</button>{" "}
+      //          </div>
+      //       ) : (
+      //          <NavLink to={"/login"}>Login</NavLink>
+      //       )}
+      //    </div>
+      // </header>
    );
 };
 
-export default Header;
+// export default Header;
